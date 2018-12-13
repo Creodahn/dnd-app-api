42f7d19f79a1b6ef0617fa584bb3cbfc79da3b05
Justin Drew <justin.a.drew@gmail.com>
2018-12-13 03:35:43 +0000

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,10 +35,10 @@ Gemstone.create(description: 'translucent dark green with black mottling and gol
 Gemstone.create(description: 'translucent lustrous black with glowing highlights', name: 'Black sapphire', value: 5000 )
 
 MagicItem.create('max': 50, 'min': 1, name: 'potion of healing', table: 'A' )
-MagicItem.create('max': 60, 'min': 51, name: 'spell scroll (cantrip)', table: 'A')
+MagicItem.create('max': 60, 'min': 51, name: 'cantrip spell scroll', table: 'A')
 MagicItem.create('max': 70, 'min': 61, name: 'potion of climbing', table: 'A')
-MagicItem.create('max': 90, 'min': 71, name: 'spell scroll (1st level)', table: 'A')
-MagicItem.create('max': 94, 'min': 91, name: 'spell scroll (2nd level)', table: 'A')
+MagicItem.create('max': 90, 'min': 71, name: '1st level spell scroll', table: 'A')
+MagicItem.create('max': 94, 'min': 91, name: '2nd level spell scroll', table: 'A')
 MagicItem.create('max': 98, 'min': 95, name: 'potion of greater healing', table: 'A')
 MagicItem.create('max': 99, 'min': 99, name: 'bag of holding', table: 'A')
 MagicItem.create( 'max': 100, 'min': 100, name: 'driftglobe', table: 'A')
