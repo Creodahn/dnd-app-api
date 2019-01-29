ed65573bde04dcba05ed1bcca2d8c6e5b2e02807
Justin Drew <justin.a.drew@gmail.com>
2019-01-29 20:51:15 +0000

    finish more magic item table seeds

---
+++ db/seeds/magic_items/table_e.rb
@@ -0,0 +1,7 @@
+MagicItem.create(max: 30, min: 1, name: '8th level spell scroll', table: 'E')
+MagicItem.create(max: 55, min: 31, name: 'potion of storm giant strength', table: 'E')
+MagicItem.create(max: 70, min: 56, name: 'potion of supreme healing', table: 'E')
+MagicItem.create(max: 85, min: 71, name: '9th level spell scroll', table: 'E')
+MagicItem.create(max: 93, min: 86, name: 'universal solvent', table: 'E')
+MagicItem.create(max: 98, min: 94, name: 'arrow of slaying', table: 'E')
+MagicItem.create(max: 100, min: 99, name: 'sovereign glue', table: 'E')
