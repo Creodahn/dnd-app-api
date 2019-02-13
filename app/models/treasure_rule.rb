c64115b801aebd0ba1e898a6ee752258131b8045
Justin Drew <justin.a.drew@gmail.com>
2019-02-13 11:28:40 -0700

    fix typos in treasure_rule_set relationship

--- app/models/treasure_rule.rb
+++ app/models/treasure_rule.rb
@@ -1,5 +1,5 @@
 class TreasureRule < ApplicationRecord
-  belongs_to :treasure_rules_set, class_name: 'TreasureRuleSets'
+  belongs_to :treasure_rule_set, class_name: 'TreasureRuleSet'
 
   has_many :dice_calculations
 end
