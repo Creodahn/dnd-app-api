5d2dc275ccf6cbd8745da27a8ccd20ef9d75dd50
Justin Drew <justin.a.drew@gmail.com>
2019-02-19 20:53:01 -0700

    add all gemstone seed data

--- db/seeds/gemstones.rb
+++ db/seeds/gemstones.rb
@@ -1,7 +1,65 @@
 puts "creating gemstones"
+
+puts "creating gemstones of value 10"
 Gemstone.create(description: 'opaque mottled deep blue', name: 'Azurite', value: 10)
+Gemstone.create(description: 'translucent striped brown, blue, white, or red', name: 'Banded agate', value: 10)
+Gemstone.create(description: 'transparent pale blue', name: 'Blue quartz', value: 10)
+Gemstone.create(description: 'translucent circles of gray, white, brown, blue, or green', name: 'Eye agate', value: 10)
+Gemstone.create(description: 'opaque gray-black', name: 'Hematite', value: 10)
+Gemstone.create(description: 'opaque light and dark blue with yellow flecks', name: 'Lapis lazuli', value: 10)
+Gemstone.create(description: 'opaque striated light and dark green', name: 'Malachite', value: 10)
+Gemstone.create(description: 'translucent pink or yellow-white with mossy gray or green markings', name: 'Moss agate', value: 10)
+Gemstone.create(description: 'opaque black', name: 'Obsidian', value: 10)
+Gemstone.create(description: 'opaque light pink', name: 'Rhodochrosite', value: 10)
+Gemstone.create(description: 'translucent brown with golden center', name: 'Tiger eye', value: 10)
+Gemstone.create(description: 'opaque light blue-green', name: 'Turquoise', value: 10)
+
+puts "creating gemstones of value 50"
 Gemstone.create(description: 'opaque dark gray with red flecks', name: 'Bloodstone', value: 50)
+Gemstone.create(description: 'opaque orange to red-brown', name: 'Carnelian', value: 50)
+Gemstone.create(description: 'opaque white', name: 'Chalcedony', value: 50)
+Gemstone.create(description: 'translucent green', name: 'Chrysoprase', value: 50)
+Gemstone.create(description: 'transparent pale yellow-brown', name: 'Citrine', value: 50)
+Gemstone.create(description: 'opaque blue, black, or brown', name: 'Jasper', value: 50)
+Gemstone.create(description: 'translucent white with pale blue glow', name: 'Moonstone', value: 50)
+Gemstone.create(description: 'opaque bands of black and white, or pure black or white', name: 'Onyx', value: 50)
+Gemstone.create(description: 'transparent white, smoky gray, or yellow', name: 'Quartz', value: 50)
+Gemstone.create(description: 'opaque bands of red and white', name: 'Sardonyx', value: 50)
+Gemstone.create(description: 'translucent rosy stone with white star-shaped center', name: 'Star rose quartz', value: 50)
+Gemstone.create(description: 'transparent pale blue-green', name: 'Zircon', value: 50)
+
+puts "creating gemstones of value 100"
 Gemstone.create(description: 'transparent watery gold to rich gold', name: 'Amber', value: 100)
+Gemstone.create(description: 'transparent deep purple', name: 'Amethyst', value: 100)
+Gemstone.create(description: 'transparent yellow-green to pale green', name: 'Chrysoberyl', value: 100)
+Gemstone.create(description: 'opaque crimson', name: 'Coral', value: 100)
+Gemstone.create(description: 'transparent red, brown-green, or violet', name: 'Garnet', value: 100)
+Gemstone.create(description: 'translucent light green, deep green, or white', name: 'Jade', value: 100)
+Gemstone.create(description: 'opaque jet black', name: 'Jet', value: 100)
+Gemstone.create(description: 'opaque lustrous white, yellow, or pink', name: 'Pearl', value: 100)
+Gemstone.create(description: 'transparent red, red-brown, or deep green', name: 'Spinel', value: 100)
+Gemstone.create(description: 'transparent pale green, blue, brown, or red', name: 'Tourmaline', value: 100)
+
+puts "creating gemstones of value 500"
 Gemstone.create(description: 'transparent dark green', name: 'Alexandrite', value: 500)
+Gemstone.create(description: 'transparent pale blue-green', name: 'Aquamarine', value: 500)
+Gemstone.create(description: 'opaque pure black', name: 'Black pearl', value: 500)
+Gemstone.create(description: 'transparent deep blue', name: 'Blue spinel', value: 500)
+Gemstone.create(description: 'transparent rich olive green', name: 'Peridot', value: 500)
+Gemstone.create(description: 'transparent golden yellow', name: 'Topaz', value: 500)
+
+puts "creating gemstones of value 1000"
 Gemstone.create(description: 'translucent dark green with black mottling and golden flecks', name: 'Black opal', value: 1000)
-Gemstone.create(description: 'translucent lustrous black with glowing highlights', name: 'Black sapphire', value: 5000 )
+Gemstone.create(description: 'transparent blue-white to medium blue', name: 'Blue sapphire', value: 1000)
+Gemstone.create(description: 'transparent deep bright green', name: 'Emerald', value: 1000)
+Gemstone.create(description: 'translucent fiery red', name: 'Fire opal', value: 1000)
+Gemstone.create(description: 'translucent pale blue with green and golden mottling', name: 'Opal', value: 1000)
+Gemstone.create(description: 'translucent ruby with white star-shaped center', name: 'Star ruby', value: 1000)
+Gemstone.create(description: 'translucent blue sapphire with white star-shaped center', name: 'Star sapphire', value: 1000)
+Gemstone.create(description: 'transparent fiery yellow or yellow-green', name: 'Yellow sapphire', value: 1000)
+
+puts "creating gemstones of value 5000"
+Gemstone.create(description: 'translucent lustrous black with glowing highlights', name: 'Black sapphire', value: 5000)
+Gemstone.create(description: 'transparent blue-white, canary, pink, brown, or blue', name: 'Diamon', value: 5000)
+Gemstone.create(description: 'transparent fiery orange', name: 'Jacinth', value: 5000)
+Gemstone.create(description: 'transparent clear red to deep crimson', name: 'Ruby', value: 5000)
