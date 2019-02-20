2772c994661667c2205e4e88ff4e90a3c3dfe662
Justin Drew <justin.a.drew@gmail.com>
2019-02-19 21:10:52 -0700

    art object seed data completed

--- db/seeds/art_objects.rb
+++ db/seeds/art_objects.rb
@@ -1,11 +1,59 @@
 puts "creating art objects"
+
+puts "creating art objects with value 25"
 ArtObject.create(name: 'Silver ewer', value: 25)
 ArtObject.create(name: 'Carved bone statuette', value: 25)
+ArtObject.create(name: 'Small gold bracelet', value: 25)
+ArtObject.create(name: 'Cloth-of-gold vestments', value: 25)
+ArtObject.create(name: 'Black velvet mask stitched with silver thread', value: 25)
+ArtObject.create(name: 'Copper chalice with silver filigree', value: 25)
+ArtObject.create(name: 'Pair of engraved bone dice', value: 25)
+ArtObject.create(name: 'Small mirror set in a painted wooden frame', value: 25)
+ArtObject.create(name: 'Embroidered silk handkerchief', value: 25)
+ArtObject.create(name: 'Gold locked with a painted portrair inside', value: 25)
+
+puts "creating art objects with value 250"
 ArtObject.create(name: 'Gold ring set with bloodstones', value: 250)
 ArtObject.create(name: 'Carved ivory statuette', value: 250)
+ArtObject.create(name: 'Large gold bracelet', value: 250)
+ArtObject.create(name: 'Silver necklace with a gemstone pendant', value: 250)
+ArtObject.create(name: 'Bronze crown', value: 250)
+ArtObject.create(name: 'Silk robe with gold embroidery', value: 250)
+ArtObject.create(name: 'Large well-made tapestry', value: 250)
+ArtObject.create(name: 'Brass mug with jade inlay', value: 250)
+ArtObject.create(name: 'Box of turquoise animal figurines', value: 250)
+ArtObject.create(name: 'Gold bird cage with electrum filigree', value: 250)
+
+puts "creating art objects with value 750"
 ArtObject.create(name: 'Silver chalice set with moonstones', value: 750)
 ArtObject.create(name: 'Silver-plated steel longsword with jet set in hilt', value: 750)
+ArtObject.create(name: 'Carved harp of exotic wood with ivory inlay and zircon gem', value: 750)
+ArtObject.create(name: 'Small gold idol', value: 750)
+ArtObject.create(name: 'Gold dragon comb set with red garnets as eyes', value: 750)
+ArtObject.create(name: 'Bottle stopper cork embossed with gold leaf and set with amethysts', value: 750)
+ArtObject.create(name: 'Ceremonial electrum dagger with a plack pearl in the pommel', value: 750)
+ArtObject.create(name: 'Silver and gold brooch', value: 750)
+ArtObject.create(name: 'Obsidian statuette with gold fittings and inlay', value: 750)
+ArtObject.create(name: 'Painted gold war mask', value: 750)
+
+puts "creating art objects with value 2,500"
 ArtObject.create(name: 'Fine gold chain set with a fire opal', value: 2500)
 ArtObject.create(name: 'Old masterpiece painting', value: 2500)
+ArtObject.create(name: 'Embroidered silk and velvet mantle set with numerous moonstones', value: 2500)
+ArtObject.create(name: 'Platinum bracelet set with a sapphire', value: 2500)
+ArtObject.create(name: 'Embroidered glove set with jewel chips', value: 2500)
+ArtObject.create(name: 'Jeweled anklet', value: 2500)
+ArtObject.create(name: 'Gold music box', value: 2500)
+ArtObject.create(name: 'Gold circlet set with four aquamarines', value: 2500)
+ArtObject.create(name: 'Eye patch with a mock eye set in a blue sapphire and moonstone', value: 2500)
+ArtObject.create(name: 'A necklace string of small pink pearls', value: 2500)
+
+puts "creating art objects with value 7,500"
 ArtObject.create(name: 'Jeweled gold crown', value: 7500)
 ArtObject.create(name: 'Jeweled platinum ring', value: 7500)
+ArtObject.create(name: 'Small gold statuette set with rubies', value: 7500)
+ArtObject.create(name: 'Gold cup set with emeralds', value: 7500)
+ArtObject.create(name: 'Gold jewelry box with platinum filigree', value: 7500)
+ArtObject.create(name: "Painted gold child's sarcophagus", value: 7500)
+ArtObject.create(name: 'Jade game board with solid gold playing pieces', value: 7500)
+ArtObject.create(name: 'Bejeweled ivory drinking horn with gold filigree', value: 7500)
